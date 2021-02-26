f66d38757ddb740e7b3e2a0d3e828568a185b49e
bhbh-00 <doe02077@naver.com>
2021-02-26 14:53:59 +0900

    74강, POST 폼 테스트

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,8 +1,12 @@
 package com.sbs.untact.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.untact.util.Util;
 
 @Controller
 public class UsrHomeController {
@@ -11,5 +15,12 @@ public class UsrHomeController {
 	// http://localhost:8024/usr/home/main
 	public String showMain() {
 		return "ㅋㅋㅋ";
+	}
+	
+	// Get과 Post 테스트
+	@RequestMapping("/usr/home/doFormTest")
+	@ResponseBody
+	public Map<String, Object> doFormTest(String name, int age) {
+		return Util.mapOf("name", name, "age", age);
 	}
 }
